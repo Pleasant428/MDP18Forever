22c5a8103784c349e64836cbf0c417ce2e721229
SaklaniPankaj <drag0n_freak@hotmail.com>
2018-09-06 01:56:53 +0800

    Slight Update

--- Algorithm/src/Simulator.java
+++ Algorithm/src/Simulator.java
@@ -12,5 +12,28 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.text.*;
 
 public class Simulator extends Application {
-
+	
+	//GUI Components
+    private int stage = 1;
+	
+	public void start(Stage primaryStage) {
+        //Setting the Title and Values for the Window
+        primaryStage.setTitle("MDP Group 18: Algorithm Simulator");
+        GridPane grid = new GridPane();
+        GridPane mapGrid = new GridPane();
+        grid.setAlignment(Pos.CENTER_LEFT);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+       
+        
+        //Font and Text Alignment
+        
+        
+        //Dimensions of the Window
+        Scene scene = new Scene(grid, 500, 450);
+        primaryStage.setScene(scene);
+        
+        //Choosing where to place components on the Grid
+        
 }
