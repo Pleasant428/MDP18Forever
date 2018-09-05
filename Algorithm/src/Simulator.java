c9b1c78e1f860f2e78f5e35f123b82212f46d42b
SaklaniPankaj <drag0n_freak@hotmail.com>
2018-09-06 01:55:04 +0800

    Base Simulator Class (2 Grid Systems, 1 Outer, 1 for the Map)

---
+++ Algorithm/src/Simulator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.collections.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.*;
+
+public class Simulator extends Application {
+
+}
